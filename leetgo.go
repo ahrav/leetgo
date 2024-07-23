7f382de485e0f0ca98dc13fed631c9a1bdfaf787
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-07-22 19:39:09 -0700

    merge sorted slices.

--- leetgo.go
+++ leetgo.go
@@ -68,3 +68,26 @@ func TwoSum(nums []int, target int) []int {
 
 	return nil
 }
+
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	i, j, k := m-1, n-1, m+n-1
+
+	for i >= 0 && j >= 0 {
+		if nums1[i] < nums2[j] {
+			nums1[k] = nums2[j]
+			j--
+		} else {
+			nums1[k] = nums1[i]
+			i--
+		}
+		k--
+	}
+
+	for j >= 0 {
+		nums1[k] = nums2[j]
+		j--
+		k--
+	}
+
+	return
+}
--- leetgo_test.go
+++ leetgo_test.go
@@ -147,3 +147,56 @@ func BenchmarkTwoSum(b *testing.B) {
 		_ = TwoSum([]int{2, 7, 11, 15}, 9)
 	}
 }
+
+func TestMerge(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums1    []int
+		m        int
+		nums2    []int
+		n        int
+		expected []int
+	}{
+		{
+			name:     "Example 1",
+			nums1:    []int{1, 2, 3, 0, 0, 0},
+			m:        3,
+			nums2:    []int{2, 5, 6},
+			n:        3,
+			expected: []int{1, 2, 2, 3, 5, 6},
+		},
+		{
+			name:     "Example 2",
+			nums1:    []int{1},
+			m:        1,
+			nums2:    []int{},
+			n:        0,
+			expected: []int{1},
+		},
+		{
+			name:     "Example 3",
+			nums1:    []int{0},
+			m:        0,
+			nums2:    []int{1},
+			n:        1,
+			expected: []int{1},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			Merge(tt.nums1, tt.m, tt.nums2, tt.n)
+			assert.Equal(t, tt.expected, tt.nums1)
+		})
+	}
+}
+
+func BenchmarkMerge(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		Merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3)
+	}
+}

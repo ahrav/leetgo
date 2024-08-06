ad9926dadc60c53653acb2c8c446ca69ab9ad0a2
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-08-05 19:12:21 -0700

    MinStack

--- leetgo.go
+++ leetgo.go
@@ -2268,3 +2268,28 @@ func MaximumUnits(boxTypes [][]int, truckSize int) int {
 
 	return total
 }
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+func MinStackConstructor() MinStack { return MinStack{} }
+
+func (s *MinStack) Push(val int) {
+	s.stack = append(s.stack, val)
+	if len(s.minStack) == 0 || val <= s.minStack[len(s.minStack)-1] {
+		s.minStack = append(s.minStack, val)
+	}
+}
+
+func (s *MinStack) Pop() {
+	if s.stack[len(s.stack)-1] == s.minStack[len(s.minStack)-1] {
+		s.minStack = s.minStack[:len(s.minStack)-1]
+	}
+	s.stack = s.stack[:len(s.stack)-1]
+}
+
+func (s *MinStack) Top() int { return s.stack[len(s.stack)-1] }
+
+func (s *MinStack) GetMin() int { return s.minStack[len(s.minStack)-1] }

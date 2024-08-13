92121f2add16812259498082ba494673e08cf274
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-08-12 18:24:03 -0700

    ReverseList

--- 75/75.go
+++ 75/75.go
@@ -647,3 +647,25 @@ func DecodeString(s string) string {
 
 	return currStr
 }
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func ReverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+
+	return prev
+}

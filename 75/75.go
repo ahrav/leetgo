ee463c360fd4650b8d0caf597dc4104ed2157473
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-08-15 19:13:08 -0700

    refactor

--- 75/75.go
+++ 75/75.go
@@ -1653,9 +1653,8 @@ func MinCostClimbingStairsDP(cost []int) int {
 
 	dp := make([]int, n)
 	dp[0], dp[1] = cost[0], cost[1]
-
 	for i := 2; i < n; i++ {
-		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+		dp[i] = cost[i] + min(dp[i-1], dp[i-2])
 	}
 
 	return min(dp[n-1], dp[n-2])

f80584c41b61010feb30e67ba2403a7da71bc824
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-08-25 15:25:53 -0700

    MinCost

--- graph/graph.go
+++ graph/graph.go
@@ -1,6 +1,9 @@
 package graph
 
-import "container/heap"
+import (
+	"container/heap"
+	"sort"
+)
 
 type Edge struct{ to, weight int }
 
@@ -66,4 +69,71 @@ func MinCostConnectPoints(points [][]int) int {
 	}
 
 	return totalCost
+}
+
+type UnionFind struct {
+	parents []int
+	ranks   []int
+}
+
+func NewUnionFind(n int) *UnionFind {
+	parents := make([]int, n)
+	ranks := make([]int, n)
+	for i := range parents {
+		parents[i] = i
+	}
+
+	return &UnionFind{parents: parents, ranks: ranks}
+}
+
+func (uf *UnionFind) Find(x int) int {
+	if uf.parents[x] != x {
+		uf.parents[x] = uf.Find(uf.parents[x])
+	}
+	return uf.parents[x]
+}
+
+func (uf *UnionFind) Union(x, y int) bool {
+	rootX, rootY := uf.Find(x), uf.Find(y)
+
+	if rootX != rootY {
+		if uf.ranks[rootX] > uf.ranks[rootY] {
+			uf.parents[rootY] = rootX
+		} else if uf.ranks[rootX] < uf.ranks[rootY] {
+			uf.parents[rootX] = rootY
+		} else {
+			uf.parents[rootY] = rootX
+			uf.ranks[rootX]++
+		}
+		return true
+	}
+
+	return false
+}
+
+// MinimumCost - https://leetcode.com/problems/connecting-cities-with-minimum-cost/
+func MinimumCost(n int, connections [][]int) int {
+	if len(connections) < n-1 { // Not enough cities to connect
+		return -1
+	}
+
+	sort.Slice(connections, func(i, j int) bool {
+		return connections[i][2] < connections[j][2]
+	})
+
+	uf := NewUnionFind(n)
+
+	totalCost, edgesUsed := 0, 0
+	for _, conn := range connections {
+		x, y, cost := conn[0]-1, conn[1]-1, conn[2]
+		if uf.Union(x, y) {
+			totalCost += cost
+			edgesUsed++
+			if edgesUsed == n-1 {
+				return totalCost
+			}
+		}
+	}
+
+	return -1
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -48,3 +48,47 @@ func BenchmarkMinCostConnectPoints(b *testing.B) {
 		})
 	}
 }
+
+func TestMinimumCost(t *testing.T) {
+	tests := []struct {
+		name        string
+		n           int
+		connections [][]int
+		expected    int
+	}{
+		{"NoConnections", 3, [][]int{}, -1},
+		{"SingleConnection", 2, [][]int{{1, 2, 5}}, 5},
+		{"MultipleConnections", 3, [][]int{{1, 2, 5}, {1, 3, 6}, {2, 3, 1}}, 6},
+		{"NotEnoughConnections", 4, [][]int{{1, 2, 5}, {1, 3, 6}}, -1},
+		{"AllCitiesConnected", 4, [][]int{{1, 2, 3}, {3, 4, 4}, {2, 3, 5}, {1, 4, 10}}, 12},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := MinimumCost(tt.n, tt.connections)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func BenchmarkMinimumCost(b *testing.B) {
+	tests := []struct {
+		name        string
+		n           int
+		connections [][]int
+	}{
+		{"NoConnections", 3, [][]int{}},
+		{"SingleConnection", 2, [][]int{{1, 2, 5}}},
+		{"MultipleConnections", 3, [][]int{{1, 2, 5}, {1, 3, 6}, {2, 3, 2}}},
+		{"NotEnoughConnections", 4, [][]int{{1, 2, 5}, {1, 3, 6}}},
+		{"AllCitiesConnected", 4, [][]int{{1, 2, 3}, {3, 4, 4}, {2, 3, 5}, {1, 4, 10}}},
+	}
+
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = MinimumCost(tt.n, tt.connections)
+			}
+		})
+	}
+}

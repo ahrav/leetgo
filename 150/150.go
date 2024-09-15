1b0a180f06fb7436055d9fe2ccba00faaa1af7db
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-09-15 16:18:22 -0700

    TwoSumSorted

--- 150/150.go
+++ 150/150.go
@@ -21,3 +21,24 @@ func MaxSubArray(nums []int) int {
 
 	return globalMax
 }
+
+// TwoSum - https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/?envType=study-plan-v2&envId=top-interview-150
+func TwoSum(numbers []int, target int) []int {
+	n := len(numbers)
+
+	lp, rp := 0, n-1
+	for lp < rp {
+		sum := numbers[lp] + numbers[rp]
+		if sum == target {
+			return []int{lp + 1, rp + 1}
+		}
+
+		if sum > target {
+			rp--
+		} else {
+			lp++
+		}
+	}
+
+	return nil // Shouldn't occur
+}
--- 150/150_test.go
+++ 150/150_test.go
@@ -34,3 +34,59 @@ func BenchmarkMaxSubArray(b *testing.B) {
 		MaxSubArray(nums)
 	}
 }
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		name     string
+		numbers  []int
+		target   int
+		expected []int
+	}{
+		{
+			name:     "NoSolution",
+			numbers:  []int{1, 2, 3, 4, 5},
+			target:   10,
+			expected: nil,
+		},
+		{
+			name:     "SinglePair",
+			numbers:  []int{2, 7, 11, 15},
+			target:   9,
+			expected: []int{1, 2},
+		},
+		{
+			name:     "MultiplePairs",
+			numbers:  []int{1, 2, 3, 4, 5, 5},
+			target:   8,
+			expected: []int{3, 6},
+		},
+		{
+			name:     "NegativeNumbers",
+			numbers:  []int{-3, -1, 0, 1, 2},
+			target:   -1,
+			expected: []int{1, 5},
+		},
+		{
+			name:     "EmptyArray",
+			numbers:  []int{},
+			target:   5,
+			expected: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			result := TwoSum(tt.numbers, tt.target)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func BenchmarkTwoSum(b *testing.B) {
+	numbers := []int{1, 2, 3, 4, 4, 5}
+	target := 8
+	for i := 0; i < b.N; i++ {
+		TwoSum(numbers, target)
+	}
+}
